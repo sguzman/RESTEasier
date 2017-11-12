d8788f642823e7b166c313cf46d01e545c6c4fcb
Salvador Guzman <guzmansalv@gmail.com>
2017-11-11 21:15:10 -0800

    removed components module

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RestuiFormComponent } from './restui-form/restui-form';
-@NgModule({
-	declarations: [RestuiFormComponent],
-	imports: [],
-	exports: [RestuiFormComponent]
-})
-export class ComponentsModule {}
