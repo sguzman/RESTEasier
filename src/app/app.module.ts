519f8ed84632e377f3ed146505e3c3fa191baf52
Salvador Guzman <guzmansalv@gmail.com>
2017-11-11 21:32:43 -0800

    add component to app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {StatusBar} from '@ionic-native/status-bar';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
 import {ReactiveFormsModule} from "@angular/forms";
+import {RestuiFormComponent} from "../components/restui-form/restui-form";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    RestuiFormComponent
   ],
   imports: [
     BrowserModule,
